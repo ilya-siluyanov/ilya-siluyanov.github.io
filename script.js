1494006051d22c39b8dc3794d06cafa093cd92d6
Ilya Siluyanov <isiluyanov8@gmail.com>
2020-03-27 21:43:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let intents = [
+    "Я уверен, что ты самый лучший на свете человек!",
+    "Я считаю, ты словно лучик солнца!",
+    "Я верю, что ты делаешь только добрые дела!"
+]
+document.forms.nameForm.onsubmit = (e)=>{
+    e.preventDefault()
+    let name = document.forms.nameForm.name.value
+    let response = document.getElementById("resp")
+    let text = intents[Math.floor(Math.random()*intents.length)]
+    if (name===""){
+        response.innerText = "Даже не зная твоего имени, "+text
+    }else{
+        if(name==="Диана"){
+            if (Math.round(Math.random())){
+                text = "Не знаю, что придумать отдельно для тебя, Диана, так что вот: "+text
+            }
+        }
+        response.innerText = text
+    }
+}
